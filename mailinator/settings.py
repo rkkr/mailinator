953047033223a56095c88543c223803a24f64f84
Raimondas Rimkus <narimantukas@gmail.com>
2015-04-23 17:48:25 +0000

    Added basic filtering to REST

--- mailinator/settings.py
+++ mailinator/settings.py
@@ -139,7 +139,11 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.DjangoFilterBackend'
+    ],
 }
 
 # A sample logging configuration. The only tangible logging
--- rest/views.py
+++ rest/views.py
@@ -1,11 +1,13 @@
 from emails.models import *
 from rest_framework import viewsets
+from rest_framework import filters
 from rest.serializers import *
 
 
 class EmailViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_fields = ('email', 'sender', 'title', 'time_stamp', 'email__email')
 
 
 class UserViewSet(viewsets.ModelViewSet):
