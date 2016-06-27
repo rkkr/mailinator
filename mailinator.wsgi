14bbb288eb5db4d800a4d3646ec3e34b13f216a1
Raimondas Rimkus <narimantukas@gmail.com>
2016-06-27 10:02:27 +0000

    Update to new django/mysql

--- mailinator.wsgi
+++ mailinator.wsgi
@@ -1,6 +1,6 @@
 import os
-import sys	
+import sys
 sys.path.append('/var/www/mailinator/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mailinator.settings'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
--- smtp_server/management/commands/smtp_server.py
+++ smtp_server/management/commands/smtp_server.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-import smtpd, asyncore, email, platform, time
+from django.db import connection
+import smtpd, asyncore, email, platform, time, datetime, sys, traceback
 from emails.models import *
 
 
@@ -9,9 +10,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
             try:
+                log('Starting smtp server')
                 server = CustomSMTPServer(('0.0.0.0', 25), None)
                 asyncore.loop()
             except:
+                log_exception()
                 time.sleep(1)
 
 
@@ -23,18 +26,31 @@ def get_body_text(part, body_str = ''):
             body_str = get_body_text(part, body_str)
     return body_str
 
+def log_exception():
+    log('Exception')
+    traceback.print_exc(file=open('/var/log/pysmtpd', 'a'))
+
+def log(message):
+    with open('/var/log/pysmtpd', 'a') as file:
+        file.write(str(datetime.datetime.now()) + ' : ' + message + '\n')
+
+
 class CustomSMTPServer(smtpd.SMTPServer):
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        msg = email.message_from_string(data)
-        body = get_body_text(msg)
+        try:
+            msg = email.message_from_string(data)
+            body = get_body_text(msg)
 
-        _sender = User.objects.get_or_create(email=mailfrom)[0]
-        for rcpt in rcpttos:
-            if rcpt.split('@')[1].split('.')[0].lower() <> platform.node().lower():
-                continue
-            _receiver = User.objects.get_or_create(email=rcpt)[0]
-            Message(email=_receiver, sender=_sender, title=msg['Subject'], body=body, peer=peer).save()
+            for rcpt in rcpttos:
+                if rcpt.split('@')[1].split('.')[0].lower() <> platform.node().lower():
+                    continue
+                _sender = User.objects.get_or_create(email=mailfrom)[0]
+                _receiver = User.objects.get_or_create(email=rcpt)[0]
+                Message(email=_receiver, sender=_sender, title=msg['Subject'], body=body, peer=peer).save()
+        except:
+            log_exception()
 
+        connection.close()
         return
 
